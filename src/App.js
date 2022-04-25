ce7fe2c024057cec4924ecd1224f2e83b9006c89
TAEIL JUNG <taeil00@nate.com>
2022-04-25 19:47:38 +0900

    영화앱 기존 구조 만들기

--- src/App.js
+++ src/App.js
@@ -1,51 +1,58 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    //console.log('add');
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    //console.log('minus');
-    this.setState(current => ({
-      count: current.count - 1,
-    }));
-  };
+  getMoives = async () => {
+    const {
+      data: {
+        data: { movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    
+    //console.log( movies);
+    this.setState({ movies, isLoading: false });
+  }
 
   componentDidMount(){
-    console.log('component rendered');
-  }
+    //영화 데이터 로딩!
+    this.getMoives();
 
-  componentDidUpdate(){
-    console.log('I just updated');
   }
-
-  componentWillUnmount(){
-    console.log('Goodbye, cruel world');
-  }
+  
 
   render(){
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading, movies } = this.state;
+    return ( <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+       )   :  (
+        <div className = "movies">
+        { movies.map((movie) => (
+              <Movie 
+                key = {movie.id}
+                id = {movie.id}
+                year = {movie.year}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+                genres = {movie.genres}
+              />
+              
+        ))}
+        </div>
+       )}
+          </section>
+       );
+        
   }
 }
 
