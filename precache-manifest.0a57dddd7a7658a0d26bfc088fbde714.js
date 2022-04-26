d3c90b7e09eff9d5e514500cd973488dc0bb9035
TAEIL JUNG <taeil00@nate.com>
2022-04-26 14:19:55 +0900

    Updates

---
+++ precache-manifest.0a57dddd7a7658a0d26bfc088fbde714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841119a69c3319acacffc75759dfd531",
+    "url": "/movie_app_2022/index.html"
+  },
+  {
+    "revision": "e25ce285450af0987170",
+    "url": "/movie_app_2022/static/css/main.97a0809a.chunk.css"
+  },
+  {
+    "revision": "b0f7d1e0df7770df3491",
+    "url": "/movie_app_2022/static/js/2.4561b9de.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/movie_app_2022/static/js/2.4561b9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25ce285450af0987170",
+    "url": "/movie_app_2022/static/js/main.6ae42e23.chunk.js"
+  },
+  {
+    "revision": "188835580e23a774fa9d",
+    "url": "/movie_app_2022/static/js/runtime-main.71c7a11f.js"
+  }
+]);
